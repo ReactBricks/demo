2d4880fe55c2780ebf746664d21b5341baf05071
Matteo Frana <f2@f2.net>
2020-03-11 16:52:21 +0100

    Added Manifest and offline for PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,19 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `React Bricks Demo website`,
+        short_name: `React Bricks Demo`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#F65A8E`,
+        display: `minimal-ui`,
+        icon: `src/images/reactbricks-icon.svg`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    {
       resolve: "gatsby-plugin-postcss",
       options: {
         postCssPlugins: [
